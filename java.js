b55e41acc298fc04815fbed25c079de087a9926f
Bahattin <72936563+BahattinCinar@users.noreply.github.com>
2022-12-31 20:59:26 +0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,1 +1,26 @@
-// JavaScript source code
+
+
+var start = 0;
+
+function zoom() {
+
+    if (start == 0) {
+
+        w = photo.width;
+        h = photo.heigth;
+
+        photo.width = photo.width * 4;
+        photo.heigth = photo.heigth * 4;
+
+        start = 1;
+
+    }
+    else {
+
+        photo.width = w;
+        photo.heigth = h;
+        start = 0;
+
+    }
+
+}
